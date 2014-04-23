afd04c9fa3409b7ae7f149558ac7e7c4a5e3e33c
Jiaxu <jia.xu@dji.com>
2014-04-23 15:32:17 +0800

    html

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+$(function() {
+
+	var xhr = new XMLHttpRequest();
+	var data_link = null;
+	var menu_btn = $('.js_menu_link');
+	var menu_cont = $('#menu_cont');
+
+	function get_link_value(el) {
+		data_link = $(el).attr("data-link");
+		return data_link;
+
+	};
+
+	menu_cont.load('page/base.html .js_ajax_page');
+	
+	menu_btn.click(function() {
+		link_value = get_link_value(this);
+		console.log(link_value);
+		//console.log(xhr);
+		menu_btn.removeClass('activate');
+		$(this).addClass('activate');
+
+		menu_cont.load('page/' + link_value + '.html .js_ajax_page',
+			function(responseTxt,statusTxt,xhr){
+			if (statusTxt=="success") {
+				console.log('加载成功');
+				return;
+			};
+			if (statusTxt=="error") {
+				//console.log('Error: '+xhr.status+': '+xhr.statusText);
+				alert("加载错误")
+			};
+		});
+		
+
+		
+	});
+
+});
